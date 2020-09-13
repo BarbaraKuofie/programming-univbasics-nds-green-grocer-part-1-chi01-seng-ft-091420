1121dfaad2b690853beb7a75dda810b75c0d6619
Barbara Kuofie <barbara.kuofie@gmail.com>
2020-09-13 23:02:42 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -28,7 +28,7 @@ def consolidate_cart(cart)
         :item =>cart[counter][:item], 
         :price => cart[counter][:price], 
         :clearance => cart[counter][:clearance], 
-        :count => 
+        :count => 0]
     end 
     counter += 1 
   end 
